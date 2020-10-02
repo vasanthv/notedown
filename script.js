94a3516173c860ab5e2637be26489d44cef335e5
Vasanth.V <imvasanthv@gmail.com>
2020-10-02 16:50:28 +0530

    Removed spaces

--- script.js
+++ script.js
@@ -82,12 +82,9 @@ function download(filename, text) {
 	var element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
 	element.setAttribute('download', filename);
-
 	element.style.display = 'none';
 	document.body.appendChild(element);
-
 	element.click();
-
 	document.body.removeChild(element);
 }
 
